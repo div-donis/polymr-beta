b5cd4da7e70175a454e691bf589cc475ec65f5ff
Don Odom <divdonis@gmail.com>
2021-12-28 18:05:15 -0800

    updated dependency

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   get '/accounts/:account_id/closed_tasks', to: 'tasks#closed_tasks_by_account'
   
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { req.format.html?  && !req.xhr?}
   
 end
